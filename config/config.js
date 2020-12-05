47461d1dfce3d5ed08ef04c3f62ee0106dd55e56
Rachel Rosenstein <rachelrosenstein1@gmail.com>
2020-12-05 16:44:18 -0500

    addmanual

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 module.exports = {
     "development": {
         "username": "root",
-        "password": process.env.DB_PASSWORD,
+        "password": "23Peaches#",
         "database": "vinylDestination_db",
         "host": "127.0.0.1",
         "dialect": "mysql"
