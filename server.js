9d55164574fb415f0f14ded8c0aa43074efa6eac
Dan ONeil <dhoneil@gmail.com>
2020-12-05 10:38:44 -0500

    Built server.js,  reformatted config.json to config.js, add dotenv

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const session = require("express-session");
+const passport = require("./config/passport");
+
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+const db = require("./models");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+  });
+});
