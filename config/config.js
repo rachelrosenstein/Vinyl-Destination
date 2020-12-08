1c7ff906a77ef19a6a50b21c7b560416de32713a
Dan O'Neil <69398269+dandandanoneil@users.noreply.github.com>
2020-12-08 15:31:19 -0500

    Update config.js
    removed password

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 module.exports = {
     "development": {
         "username": "root",
-        "password": "23Peaches#",
+        "password": process.env.DB_PASSWORD,
         "database": "vinylDestination_db",
         "host": "127.0.0.1",
         "dialect": "mysql"
