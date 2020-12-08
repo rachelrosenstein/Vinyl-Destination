9d60115faf93f2fd975c38b6304243218496d9a4
Roman Mazzei <mazzeiroman@gmail.com>
2020-12-08 15:30:27 -0500

    login situation fixed

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 const db = require("./models");
 
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
