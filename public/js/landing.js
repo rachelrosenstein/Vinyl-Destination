cf1dfe379a98f9d5b110ea7f14b24dc5abe5dac4
Roman Mazzei <mazzeiroman@gmail.com>
2020-12-10 22:26:56 -0500

    added stream and addtofavorites buttons

--- public/js/landing.js
+++ public/js/landing.js
@@ -7,9 +7,15 @@ $(document).ready(function () {
         method: "GET"
     }).then(function (response) {
         console.log(response)
+
         let newImgDiv = "";
+        let bigImg = "";
         let title = "";
         let artist = "";
+        let addTo = "";
+        let stream = "";
+
+
         for (let i = 0; i <= 7; i++) {
             // let imgSrc = response.albums.album[i].image[2]["#text"];
             let imgSrc;
@@ -25,105 +31,42 @@ $(document).ready(function () {
             newImgDiv = '<img src=' + "'" + imgSrc + "'" + '">';
 
             $('.n' + i).append(newImgDiv);
+            //============================================================
+            $(document).on('click', '.n' + i, function (event) {
+                $(".albumTitle").empty();
+                $(".artist").empty();
+                $(".bigImg").empty();
+
+                console.log(response.albums.album[i].artist.name)
+
+                title = response.albums.album[i].name;
+                artist = response.albums.album[i].artist.name;
+
+                addTo = $("<button> Add to Favorites </button>")
+                addTo.attr("type", "button");
+                addTo.attr("class", "addTo");
+                stream = $("<button> stream </button>");
+                stream.attr("type", "button")
+                stream.attr("class", "stream");
+                $(".btn2").attr("action", "/"+response.albums.album[i].url);
+                bigImg = '<img src=' + "'" + imgSrc + "'" + '">';
+
+                $(".albumTitle").append(title);
+                $(".artist").append(artist);
+                $(".bigImg").append(bigImg)
+                $(".btn1").append(addTo);
+                $(".btn2").append(stream);
+
+                $(".addTo").on("click", function () {
+                    window.location.replace("/login");
+                });
+                $(".stream").on("click", function () {
+                    window.location.replace(response.albums.album[i].url);
+                });
+            })
+
         }
     })
 })
-            //=================================================
 
-    //         $(document).on('click', '.n' + i, function (event) {
-    //             const album = $('#searchBar').val();
-    //             const queryURL = 'http://ws.audioscrobbler.com/2.0/?method=album.search&album=' + album + '&api_key=7a06fd430e2d699b85d6ce8f8043cc7e&format=json';
-
-    //             $.ajax({
-    //                 url: queryURL,
-    //                 method: 'GET'
-    //             }).then(function (response) {
-    //                 let results = response.results.albummatches.album;
-
-    //                 // Iterate through the result and build a cleaner array of objects representing each search result
-    //                 for (let i = 0; i < results.length; i++) {
-    //                     let newAlbum = {
-    //                         name: results[i].name,
-    //                         artist: results[i].artist,
-    //                         imageURL: results[i].image[2]['#text'],
-    //                         mbid: results[i].mbid
-    //                     }
-    //                     searchResults.push(newAlbum);
-    //                 }
-
-    //                 let limit = 12;
-    //                 if (searchResults.length < 12) limit = searchResults.length;
-
-    //                 $('#resultHeading').append($('<div class="col-12"><h4>Results from <a href="https://www.last.fm/home" target="blank">last.fm</a>:</h4></div>'));
-
-    //                 for (let i = 0; i < limit; i++) {
-    //                     let thisDiv = $('<div class="results col-lg-3 col-sm-4 col-xs-6"></div>');
-    //                     thisDiv.attr('id', 'album');
-
-    //                     thisDiv.append($("<hr/>"))
-
-    //                     let nameDiv = $('<div><strong>' + searchResults[i].name + '</strong></div>');
-    //                     thisDiv.append(nameDiv);
-
-    //                     let artistDiv = $('<div><em>by ' + searchResults[i].artist + '</em></div>');
-    //                     thisDiv.append(artistDiv);
-
-    //                     let albumArt = $('<img alt="' + searchResults[i].name + ' album cover">')
-    //                     if (searchResults[i].imageURL) albumArt.attr('src', searchResults[i].imageURL);
-    //                     else albumArt.attr('src', 'https://via.placeholder.com/164x174/fee500?text=Cover%20Art%20Not%20Found');
-    //                     albumArt.click(function () { loadAlbum(searchResults[i].mbid) });
-    //                     thisDiv.append(albumArt);
-
-    //                     $('#resultBody').append(thisDiv);
-    //                 }
-
-    //                 $('#resultHeading').removeClass('hide');
-    //                 $('#resultBody').removeClass('hide');
-    //             });
-    //         });
-    //     }
-    // })
-// })
-
-//                 albumInf.empty();
-//                 $(".tracks").empty();
-//                 $(".tags").empty();
-
-//                 title = "<h1>" + res.name + "</h1>";
-//                 artist = "<h3>" + res.artist.name + "</h3>";
-//                 addTo= "<button> Add To Your Library </button>";
-//                 albumInf.append(title)
-//                 albumInf.append(artist)
-//                 albumInf.append(addTo)
-
-
-//                 const queryURLinfo = "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key="+apiKey+ "&artist="+res.artist.name+ "&album="+res.name+"&format=json";
-
-//                 $.ajax({
-//                     url: queryURLinfo,
-//                     method: "GET"
-//                 }).then(function(resp){
-//                 summ = resp.album.url;
-//                 console.log(summ);
-//                 // let track = resp.album.tracks.track[i];
-//                 // track = ("<li>" + resp.album.tracks.track[i].name + "</li>")
-//                 moreInfo= "<button> info </button>";
-//                 // moreInfo.attr("type","button");
-//                 // moreInfo.addClass("btn btn-warning")
-//                 // albumInf.append(addTo);
-//                 for (let i = 0; i <= 10; i++){
-//                     // console.log(resp.album.tags.tag[i].name);
-//                     tracks = ("<ul>" + resp.album.tracks.track[i].name + "</ul>")
-//                     $(".tracks").append(tracks);
-//                 }
-//                 for (let i = 0; i <= 3; i++){
-//                     console.log(resp.album.tags.tag[i]);
-//                     tags = ("<ul>" + resp.album.tags.tag[i].name + "</ul>")
-//                     $(".tags").append(tags);
-//                 }
-//                 })
-//             });
-//         }
-//     })
-// });
 
